9afc660fef17d89f684a3d88fdfebf00b710bbeb
tudaolw <daylatu951@gmail.com>
2025-12-19 16:26:48 +0700

    init project

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "YOUR_PROJECT_ID_HERE" # Copy this from GCP Console
+  region  = "us-central1"
+}
